6f91a92d4034d3c00ec71e521d494fa9e7ee2e5d
cbrghostrider <sourabh.s.joshi@gmail.com>
2015-03-30 22:02:53 -0700

    Removed file that wasn't mine.

--- Haskell/Euler/sieve.hs
+++
@@ -1,6 +0,0 @@
-main = print $ solve where
-  solve = sum $ primes 2000000 where
-    primes n = sieve $ 2:[3,5..n] where
-      sieve (p:xs) | p*p > n = xs
-                   | True = p:sieve [x | x <- xs, mod x p /= 0]
-
