1bc0793253bc94925d37ee4cc5d18c58d7f55a28
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-04-01 23:04:00 -0700

    prob 4 naive soln.

---
+++ leetcode/4_medianSorted.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        int R[m+n] = {0};
+        int ri = 0, ai=0, bi=0;
+        while (ri < m+n) {
+            if (ai >= m && bi >= n) {
+                assert(0); //impossible
+            } else if (ai >= m) {
+                    R[ri++] = B[bi++];                
+            } else if (bi >= n) {
+                    R[ri++] = A[ai++];                
+            } else {
+                if (A[ai] < B[bi]) {
+                    R[ri++] = A[ai++];
+                } else {
+                    R[ri++] = B[bi++];
+                }
+            }
+        }
+        
+        if ((m+n) % 2) {
+            return R[((m+n)/2)];
+        } else {
+            return ((double)(R[((m+n)/2) - 1] + R[((m+n)/2)])) / 2.0;
+        }
+    }
+};
+
