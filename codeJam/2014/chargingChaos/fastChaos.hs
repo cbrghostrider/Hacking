d55d24543ba48332dcc08a1ca57ef7cd71f9aafc
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-04-14 18:01:26 -0700

    A couple more problems.

---
+++ codeJam/2014/chargingChaos/fastChaos.hs
@@ -0,0 +1,74 @@
+import System.Environment (getArgs)
+import Data.Ord
+import Data.List
+import Data.Maybe 
+
+data Bit = Zero | One deriving (Eq, Ord)
+type Flow = [Bit]
+type Problem = (Int, Int, [Flow], [Flow])
+type Result  = Maybe Int 
+
+
+main :: IO ()
+main = do
+  [file]  <- getArgs
+  ip <- readFile file
+  writeFile ((takeWhile (/= '.') file) ++ ".out" ) (processInput ip)
+
+writeOutput :: [(Int, Result)] -> [String]
+writeOutput = map (\(i, r) -> ("Case #" ++ (show i) ++ ": " ++ (writeResult r)))
+
+processInput :: String -> String
+processInput = unlines . writeOutput . zip [1..] . map solveProblem . parseProblem . tail . lines
+
+writeResult :: Result -> String
+writeResult Nothing  = "NOT POSSIBLE"
+writeResult (Just x) = show x
+
+readBit :: Char -> Bit
+readBit '0' = Zero
+readBit '1' = One
+
+parseProblem :: [String] -> [Problem]
+parseProblem [] = []
+parseProblem (nl:os:ds:ss) = ((read . head . words $ nl), (read . last . words $ nl), 
+                               (map (map readBit) . words $ os), (map (map readBit) . words $ ds)) : parseProblem ss
+
+sortFlips :: [Bit] -> [Bit] -> Ordering
+sortFlips [] [] = EQ
+sortFlips [] _  = error "unequal lengths"
+sortFlips _  [] = error "unequal lengths"
+sortFlips (l:ls) (r:rs)
+  | numTrue (l:ls) < numTrue (r:rs) = LT
+  | numTrue (l:ls) > numTrue (r:rs) = GT
+  | l < r  = LT
+  | l > r  = GT
+  | l == r = sortFlips ls rs
+    where numTrue = length . filter (== One)
+
+xorBits :: Bit -> Bit -> Bit
+xorBits Zero Zero = Zero
+xorBits Zero One  = One
+xorBits One Zero  = One
+xorBits One One   = Zero
+
+xorFlows :: Flow -> Flow -> Flow
+xorFlows f1 f2 = map (\(b1, b2) -> b1 `xorBits` b2) $ zip f1 f2
+
+-- takes the first device flow, and a list of outlet flows
+-- returns a list of flip strings, that can cause flows to match
+-- sorts the candidates before returning, in terms of number of least # Ones
+genFlowCandidates :: Flow -> [Flow] -> [Flow]
+genFlowCandidates seq = sortBy (sortFlips) . map (xorFlows seq)
+
+solveProblem :: Problem -> Result
+solveProblem pb@(n, l, oss, dss) = 
+  let flipCands = genFlowCandidates (head dss) oss
+      osss'     = map sort . map (\fc -> map (xorFlows fc) oss) $ flipCands
+      dss'      = sort dss
+      matches   = filter ((==True) . snd) . zip flipCands . map (== dss') $ osss'
+  in  if null matches 
+         then Nothing
+         else Just (length . filter (== One) . fst . head $ matches)
+
+
---
+++ codeJam/2014/shuffle/shuffle.hs
@@ -0,0 +1,45 @@
+import System.Environment (getArgs)
+import Data.Ord
+import Data.List
+import Data.Maybe 
+
+-- FIXME: Change these
+type Problem = [Int]
+type Result  = Bool
+
+main :: IO ()
+main = do
+  [file]  <- getArgs
+  ip <- readFile file
+  writeFile ((takeWhile (/= '.') file) ++ ".out" ) (processInput ip)
+
+writeOutput :: [(Int, Result)] -> [String]
+writeOutput = map (\(i, r) -> ("Case #" ++ (show i) ++ ": " ++ (writeResult r)))
+
+processInput :: String -> String
+processInput = unlines . writeOutput . zip [1..] . map solveProblem . parseProblem . tail . lines
+
+writeResult :: Result -> String
+writeResult True  = "GOOD"
+writeResult False = "BAD"
+
+parseProblem :: [String] -> [Problem]
+parseProblem [] = []
+parseProblem (_:ns:ss) = (map read . words $ ns) : parseProblem ss
+
+-- all numbers have equal probability of being found at a position
+-- so about half the numbers are at indices bigger, and other half smaller
+numGood :: Int
+numGood = 500
+
+-- this is skewed 
+numBad :: Int
+numBad  = 472
+
+solveProblem :: Problem -> Result
+solveProblem ns = 
+  let numgt = length . filter (==True) . map (\(i, v) -> if v <= i then True else False) . zip [0..] $ ns
+      midpt = numBad + (numGood - numBad) `div` 2
+  in  if numgt >= midpt then True else False
+
+
