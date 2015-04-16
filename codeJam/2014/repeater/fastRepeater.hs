dd7a01b6bebe3bb14a22fa113323363ed213fe32
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-04-16 14:10:16 -0700

    Solved repeater from round 1B in 2014.
    Used dynamic programming for my own solution, which works fine.
    Then read the analysis, realized that another much simpler solution is
    presented, and coded that for fastRepeater.

---
+++ codeJam/2014/repeater/fastRepeater.hs
@@ -0,0 +1,65 @@
+import System.Environment (getArgs)
+import Data.Ord
+import Data.List
+import Data.Maybe 
+import Data.Array
+
+type Problem = [String]
+type Result  = Maybe Int
+type Rle     = [(Int, Char)] -- Run length encoding to represent each string
+
+main :: IO ()
+main = do
+  [file]  <- getArgs
+  ip <- readFile file
+  writeFile ((takeWhile (/= '.') file) ++ ".out" ) (processInput ip)
+
+writeOutput :: [(Int, Result)] -> [String]
+writeOutput = map (\(i, r) -> ("Case #" ++ (show i) ++ ": " ++ (writeResult r)))
+
+processInput :: String -> String
+processInput = unlines . writeOutput . zip [1..] . map solveProblem . parseProblem . tail . lines
+
+writeResult :: Result -> String
+writeResult Nothing  = "Fegla Won"
+writeResult (Just x) = show x
+
+parseProblem :: [String] -> [Problem]
+parseProblem [] = []
+parseProblem (n:ss)  = (take num ss) : (parseProblem (drop num ss))
+  where num = read n
+
+convertRle :: String -> Rle
+convertRle [] = []
+convertRle ls = foldr (\cl ((num, letter):rles) -> if cl == letter then ((num+1, letter):rles) else ((1, cl):(num, letter):rles)) ([(1, l)]) $ ls'
+  where l   = last ls
+        ls' = init ls
+
+possibleConv :: Rle -> Rle -> Bool
+possibleConv rle1 rle2 = if length rle1 /= length rle2 then False else all (==True) . map (\((_, ch1), (_, ch2)) -> if ch1 == ch2 then True else False) $ zip rle1 rle2
+
+impossible :: [Rle] -> Bool
+impossible [] = True
+impossible rles = any (==False) . map (possibleConv (head rles)) $ tail rles
+
+getSubStr :: (Rle -> a) -> [Rle] -> [a]
+getSubStr _ [] = []
+getSubStr f rles = if all (==[]) rles then [] else map f rles
+
+findConvCount :: [Rle] -> Int
+findConvCount [] = 0
+findConvCount rles = 
+  let hrle  = sortBy (comparing fst) . getSubStr head $ rles
+      trles = getSubStr tail rles
+      mid   = ((length hrle) `div` 2) 
+      median= if length hrle `mod` 2 == 1 then (fst $ hrle !! mid) else ((fst $ hrle !! (mid-1)) + (fst $ hrle !! (mid))) `div` 2
+      sumofabsdiffs = sum . map (abs . (subtract median) . fst) $ hrle
+  in sumofabsdiffs + findConvCount trles
+
+minConversions :: [Rle] -> Result
+minConversions rles = if null rles || impossible rles then Nothing else Just (findConvCount rles)
+
+solveProblem :: Problem -> Result
+solveProblem = minConversions . map convertRle 
+
+
---
+++ codeJam/2014/repeater/repeater_small_dynProg.hs
@@ -0,0 +1,71 @@
+-- Lazy Dynamic Programming to solve problem
+-- https://code.google.com/codejam/contest/2994486/dashboard#s=p0&a=0
+import System.Environment (getArgs)
+import Data.Ord
+import Data.List
+import Data.Maybe 
+import Data.Array
+
+type Problem = [String]
+type Result  = Maybe Int 
+
+main :: IO ()
+main = do
+  [file]  <- getArgs
+  ip <- readFile file
+  writeFile ((takeWhile (/= '.') file) ++ ".out" ) (processInput ip)
+
+writeOutput :: [(Int, Result)] -> [String]
+writeOutput = map (\(i, r) -> ("Case #" ++ (show i) ++ ": " ++ (writeResult r)))
+
+processInput :: String -> String
+processInput = unlines . writeOutput . zip [1..] . map solveProblem . parseProblem . tail . lines
+
+writeResult :: Result -> String
+writeResult Nothing  = "Fegla Won"
+writeResult (Just x) = show x
+
+parseProblem :: [String] -> [Problem]
+parseProblem [] = []
+parseProblem (n:ss)  = (take num ss) : (parseProblem (drop num ss))
+  where num = read n
+
+findResult :: String -> String -> Result
+findResult s1in s2in =
+  let s1 = ('#': s1in) ++ ['$'] -- pad start and end
+      s2 = ('@': s2in) ++ ['&'] -- pad start and end
+      go :: Int -> Int -> Result
+      go i j 
+        | i == 0 && j == 0         = Just 0    -- matched all future ones by add/delete
+        | i == 0                   = if (s2 !! j) == (s2 !! (j+1)) 
+                                        then (arr ! (i, j-1)) >>= \x -> Just (1+x)
+                                        else Nothing
+        | j == 0                   = if (s1 !! i) == (s1 !! (i+1)) 
+                                        then (arr ! (i-1, j)) >>= \x -> Just (1+x)
+                                        else Nothing
+        | (s1 !! i) == (s2 !! j)   = arr ! (i-1, j-1) -- don't need add or delete
+        | otherwise                = let c1 = if (s1 !! (i+1)) == (s1 !! i) -- delete case
+                                                 then (arr ! (i-1, j)) >>= \x -> Just (x + 1)
+                                                 else Nothing
+                                         c2 = if (s2 !! (j+1)) == (s2 !! j) -- delete case
+                                                 then (arr ! (i, j-1)) >>= \x -> Just (x + 1)
+                                                 else Nothing
+                                         c3 = if (s1 !! (i+1)) == (s2 !! j) -- add case
+                                                 then (arr ! (i, j-1)) >>= \x -> Just (x + 1)
+                                                 else Nothing
+                                         c4 = if (s2 !! (j+1)) == (s1 !! i) -- add case
+                                                 then (arr ! (i-1, j)) >>= \x -> Just (x + 1)
+                                                 else Nothing
+                                         cs = [c1, c2, c3, c4] 
+                                         cs'= map (fromMaybe 0) . filter (/= Nothing) $ cs
+                                     in  if null cs' then Nothing else (Just . minimum $ cs')
+      b1 = length s1
+      b2 = length s2
+      bounds = ((0, 0), (b1, b2))
+      arr = listArray bounds [go i j | i <- [0..b1], j <- [0..b2]] 
+  in  go (length s1in) (length s2in)
+
+solveProblem :: Problem -> Result
+solveProblem pb = findResult (pb !! 0) (pb !! 1)
+
+
