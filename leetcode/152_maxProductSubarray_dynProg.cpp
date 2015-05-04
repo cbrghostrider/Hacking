84a367ff8b8cf9bef55886652bcd5dafc1d988c8
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-05-03 19:49:58 -0700

    Couple more problems.

---
+++ leetcode/152_maxProductSubarray_dynProg.cpp
@@ -0,0 +1,48 @@
+//Two tricks for space optimization:
+//Trick 1: Since I only use last sz array, I can keep just a 2 dimensional array with first dimension[2]
+//Trick 2: Since 1 is identity for multiplication, after recording max of array, 
+//         and before dynamic programming I can delete all 1s
+class Solution {
+public:
+    int maxProduct(vector<int>& numbers) {
+        int max=std::numeric_limits<int>::min();     
+        
+        //remove all the 1s since 1 is identity for multiplication
+        vector<int> nums;
+        for (unsigned int i=0; i<numbers.size(); i++) {
+            if (numbers[i] > max) max = numbers[i]; //do this before deleting the 1s
+            if (numbers[i] != 1) nums.push_back(numbers[i]); 
+        }
+        if (nums.size() == 0) {
+            return max;
+        }
+        
+        //dynamic programming cache; c[i][j] is product of contiguous elements i starting at index j
+        int **cache = new int*[2];
+        cache[0] = new int[nums.size()];
+        cache[1] = new int[nums.size()];
+        
+        //initialization
+        for (unsigned int j=0; j<nums.size(); j++) {
+            cache[1][j] = std::numeric_limits<int>::min();
+            cache[0][j] = nums[j]; 
+        }
+        
+        int prev = 0;
+        int next = 1;
+        
+        //run dynamic programming alg
+        for (unsigned int sz=2; sz<=nums.size(); sz++) {
+            for (unsigned int i=0; i<nums.size() && (i+sz-1) < nums.size(); i++) {
+                cache[next][i] = cache[prev][i+1] * nums[i]; 
+                if (cache[next][i] > max) max = cache[next][i];
+            }
+            prev = 1 - prev;
+            next = 1 - next;
+        }
+        
+        //free memory
+        delete cache[1]; delete cache[0]; delete cache;
+        return max;
+    }
+};
---
+++ leetcode/153_minRotatedArray_BigOh_logn.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isMinElement(vector<int>& nums, int i) { //compared to both my neighbors, am I min
+        bool evalPrev = (i == 0) ? (nums[i] <= nums[nums.size()-1]) : (nums[i] < nums[i-1]);
+        bool evalNext = (i == (nums.size() - 1)) ? (nums[i] <= nums[0]) : (nums[i] < nums[i+1]);
+        if (evalPrev && evalNext) return true ; else return false;
+    }
+    
+    int binarySearchMinima(vector<int>& nums, int lb, int ub) {
+        if ((lb == ub) || ((lb + 1) == ub)) { //base cases
+            if (isMinElement(nums, lb)) {
+                return nums[lb];
+            } else if (isMinElement(nums, ub)) {
+                return nums[ub];
+            }
+            return -1; // error?
+        }
+        int mid = (lb + ub)/2;
+        if (isMinElement(nums, mid)) {
+            return nums[mid];
+        } else if (nums[mid] > nums[ub]) {
+            return binarySearchMinima(nums, mid, ub);
+        } else {
+            return binarySearchMinima(nums, lb, mid);
+        }
+    }
+    int findMin(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return -1; //undefined
+        }
+        return binarySearchMinima(nums, 0, nums.size()-1);
+    }
+};
