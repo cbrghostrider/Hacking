144edd4a98600271118f800e4089e9f814edf8d0
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-05-04 23:04:24 -0700

    Word break.

---
+++ leetcode/139_wordBreak_dynProg_complex.cpp
@@ -0,0 +1,58 @@
+//Too complicated, because it uses a 2-D array
+//Time-Limit-Exceeded
+//Other solution is simpler!
+class Solution {
+private:
+    bool **isWord_;
+    void runDynamicProg_(string s, unordered_set<string>& wordDict) {
+        //allocate cache
+        isWord_ = new bool*[s.length()];
+        for (unsigned int i=0; i<s.length(); i++) {
+            isWord_[i] = new bool[s.length()];
+        }
+        
+        //main dyn programming algorithm
+        for (unsigned int sz=1; sz <= s.length(); sz++) {
+            unsigned int szInd = sz-1;
+            for (unsigned int i=0; i<s.length() && (i+sz-1)<s.length(); i++) { //i denotes starting index
+                string testMe = s.substr(i, sz);
+                
+                //Is word of size sz starting at index i in s, a legit word or not?
+                isWord_[szInd][i] = (wordDict.find(testMe) == wordDict.end()) ? false : true;
+            }
+        }
+    }
+    
+    void freeDynamicProgCache_(string s) {
+        for (unsigned int i=0; i<s.length(); i++) {
+            delete [] isWord_[i];
+        }
+        delete [] isWord_;
+    }
+    
+    //do letters between [start, end] inclusive, form a legit phrase
+    bool isLegitPhrase_(unsigned int start, unsigned int end) {
+        
+        //either this entire thing is a word
+        if (isWord_[end-start+1-1][start]) return true;
+        
+        //single letter phrase didn't exist, so no more recursion
+        if (start == end) return false; 
+            
+        //or it is possibly composed of some sequence of words
+        for (unsigned int i=start; i<end; i++) {
+            if (isLegitPhrase_(start, i) && isLegitPhrase_(i+1, end)) return true;
+        }
+        
+        //no dice... not a legit phrase
+        return false;
+    }
+
+public:
+    bool wordBreak(string s, unordered_set<string>& wordDict) {
+        runDynamicProg_(s, wordDict);
+        bool retval = isLegitPhrase_(0, s.length()-1);
+        freeDynamicProgCache_(s);
+        return retval;
+    }
+};
---
+++ leetcode/139_wordBreak_dynProg_simpler.cpp
@@ -0,0 +1,35 @@
+//simpler dynamic programming solution, using only 1-D array
+
+class Solution {
+private:
+    bool *isWord_; 
+public:
+    bool wordBreak(string s, unordered_set<string>& wordDict) {
+        //allocate cache
+        isWord_ = new bool[s.length()];
+        
+        //base case- is [0, 0] a word?
+        isWord_[0] = (wordDict.find(s.substr(0, 1)) == wordDict.end()) ? false : true;
+        
+        //main dynamic programming algorithm
+        for (unsigned int i=1; i<s.length(); i++) { 
+                // is [0,i] one huge word?
+                bool isWord = (wordDict.find(s.substr(0, i+1)) == wordDict.end()) ? false : true;
+                
+                for (unsigned int j=0; j<i; j++) {
+                    //if [0, j] is a bunch of words, and [j+1, i] is a word, we are good!
+                    bool isWord_jp1_i = (wordDict.find(s.substr(j+1, i-j)) == wordDict.end()) ? false : true;
+                    if (isWord_[j] && isWord_jp1_i) {
+                        isWord = true;
+                        break;
+                    }
+                }
+                
+                //Is phrase starting from the beginning, and ending at i a word or collection of words?
+                isWord_[i] = isWord;
+        }
+        bool retval = isWord_[s.length()-1];
+        delete [] isWord_;
+        return retval;
+    }
+};
