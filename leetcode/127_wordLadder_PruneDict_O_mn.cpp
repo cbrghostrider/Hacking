c8e029dd37774c68526fc54ca4ddeff7d34b1428
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-05-13 20:56:30 -0700

    Word ladder, using graph and ad-hoc solutions.

---
+++ leetcode/127_wordLadder_PruneDict_O_mn.cpp
@@ -0,0 +1,77 @@
+// this one was accepted by online judge
+// The prev algorithm was too slow. 
+// This algorithm first enters the start and end words in dict.
+// Then it begins with the start word, and tries each position, in sequence
+//    during each position, it changes the letter into other letters (~26 tries)
+//    if this new candidate is found to be in dictionary, it is put in a list 
+//    (and deleted from the dictionary to prevent back-and-forth thrashing)
+//    its distance from the prev is also recorded
+//    in the end, either we find the end word and min-distance, or exit with no path.
+// time complexity:
+// Assuming max word length is m, this will try 26*m combinations for each word
+// Assuming n words, complexity is O(mn)
+// This is better than the previous because the length of any word is usually
+// much smaller than the length of the dictionary!
+class Solution {
+    class Node {
+    public:
+        string me;
+        int val;
+        Node(const string& str) : me(str), val(0){}
+    };
+
+    vector<char> alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                             'u', 'v', 'w', 'x', 'y', 'z'};
+    
+public:
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
+        
+        //check edge cases
+        if (beginWord == endWord) {
+            return 0; //no transformation possible, or trivial identity transformation
+        }
+
+        wordDict.insert(beginWord);
+        wordDict.insert(endWord);
+
+        Node *begin = new Node(beginWord);
+	begin->val = 1;
+        wordDict.erase(beginWord);
+        int pathLen=0;
+        bool done = false;
+        deque<Node*> que(1, begin);
+        while (!que.empty() && !done) {
+            Node *exp = que.front(); que.pop_front();
+            string word = exp->me;
+            int myval   = exp->val;
+
+            for (int i=0; i<word.length() && !done; i++) {
+                for (int k=0; k<alphabet.size(); k++) {
+                    string newstr = word.substr(0, i) + string(1, alphabet[k]) + word.substr(i+1);
+                    assert(newstr.size() == word.size());
+                    if (wordDict.find(newstr) == wordDict.end()) continue;
+
+                    Node *newNd = new Node(newstr);
+                    newNd->val = myval + 1;
+                    que.push_back(newNd);
+                    wordDict.erase(newstr);
+                    if (newstr == endWord) {
+                        pathLen = newNd->val;
+                        done = true;
+                        break;
+                    }
+                }
+            }
+            delete exp;
+        }
+
+        //unpopped items to be deleted in case of early exit
+        for (Node*& nd : que) {
+            delete nd;
+        }
+        
+        return pathLen;
+    }
+};
+
---
+++ leetcode/127_wordLadder_graph_O_nsqr.cpp
@@ -0,0 +1,108 @@
+//solution currently getting TLE on big inputs
+//
+//This version constructed a graph from all the words in the dictionary.
+//Two nodes in the graph were connected with an edge if they differed in exactly one spot.
+//The start and end words were also added to the graph, w/ proper edges.
+//Then a BFS from start node was conducted, which gave the shortest path to the end node
+//
+//The time complexity of this algorithm is:
+//Assuming: n words in dictionary
+//Graph construction took O(n^2) to check every word against every other
+//BFS is usually O(m+n) for m vertices and n edges
+//depending on the input, each word could potentially be convertible into each other
+//so this could be O(n^2) edges as well
+//time complexity: O(n^2)
+class Solution {
+    //my neighbor string is one that differs from me in just one position
+    bool isNeighbor_(const string& lhs, const string& rhs) {
+        assert(lhs.length() == rhs.length());
+        
+        if (lhs == rhs || lhs.length() == 0) return false;       //same item 
+        int diffCnt=0;
+        for (int i=0; i<lhs.length(); i++) {
+            if (lhs[i] != rhs[i]) {
+                diffCnt++;
+            }
+        }
+        return (diffCnt == 1 ? true : false);
+    }
+    
+    class Node {
+    public:
+        string me;
+        int val;
+        bool explored;
+        unordered_set<Node*> neighbors;
+        
+        Node(const string& str) : me(str), val(0), explored(false), neighbors() {}
+        bool hasNeighbor(Node *const check) { return (neighbors.find(check) != neighbors.end());}
+        void addNeighbor(Node *const nb) {if (!hasNeighbor(nb)) neighbors.insert(nb);}
+    };
+    
+    void clearMarks() {
+        for (Node* const& nd : words) {
+            nd->val=0;
+            nd->explored = false;
+        }
+    }
+    
+    unordered_set<Node*> words; //dictionary as nodes
+public:
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
+        
+        //assign nodes to all words in dict
+        for (const string& str : wordDict) {
+            words.insert(new Node(str));
+        }
+        
+        //add nodes for check strings
+        Node *beginnode = new Node(beginWord);
+        Node *endnode   = new Node(endWord);
+        words.insert(beginnode);
+        words.insert(endnode);
+        
+        //create graph
+        for (Node* const& ndlhs : words) {
+            for (Node* const& ndrhs : words) {
+                if (isNeighbor_(ndlhs->me, ndrhs->me)) {
+                    ndlhs->addNeighbor(ndrhs);
+                    ndrhs->addNeighbor(ndlhs);
+                }
+            }
+        }
+        
+        //check edge cases
+        auto bit = words.find(beginnode);
+        auto eit = words.find(endnode);
+        if ((*bit)->neighbors.size() == 0 || (*eit)->neighbors.size()  == 0 || beginWord == endWord) {
+                return 0; //no transformation possible, or trivial identity transformation
+        }
+        
+        int pathLen=0;
+        bool done = false;
+        deque<Node*> que(1, beginnode);
+        //breadth first search to get path length
+        while (!que.empty() && !done) {
+            Node* exp = que.front(); que.pop_front();
+            exp->explored = true;
+            
+            for (Node *const& nd : exp->neighbors) {
+                if (nd->explored) continue;
+                nd->val = exp->val+1;
+                que.push_back(nd);
+                if (nd == endnode) {
+                    done = true;
+                    pathLen = nd->val;
+                    break;
+                }
+            }
+        }
+        
+        //free up the nodes
+        for (Node *const& nd : words) {
+            delete nd;
+        }
+        
+        return (pathLen == 0 ? 0 : pathLen + 1);
+    }
+};
