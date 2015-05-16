a7d484df13e013c0cd7a2cb535f082d91ba3782a
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-05-16 09:56:51 -0700

    Word search in a grid.

---
+++ leetcode/079_wordSearch.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    typedef pair<int, int> Coord;
+    
+    bool onBoard(Coord coord, int dimr, int dimc) {
+        return (coord.first >=0 && coord.first <dimr && coord.second>=0 && coord.second<dimc);
+    }
+    
+    bool traverse(vector<vector<char>>& board, vector<vector<bool>>& grid, Coord coord, string word, int index) {
+        if (index >= word.length()) return true;
+        vector<Coord> neighbors = {make_pair(coord.first-1, coord.second), make_pair(coord.first+1, coord.second), 
+                                   make_pair(coord.first, coord.second-1), make_pair(coord.first, coord.second+1)};
+        for (Coord& nb : neighbors) {
+            if (!onBoard(nb, board.size(), board[0].size()) || grid[nb.first][nb.second] == true || 
+                 board[nb.first][nb.second] != word[index]) continue;
+            grid[nb.first][nb.second] = true;
+            if (traverse(board, grid, nb, word, index+1)) return true;
+            grid[nb.first][nb.second] = false;
+        }
+        return false;
+    }
+    
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if (board.size() == 0 || board[0].size() == 0 || word.length() == 0) return false;
+        
+        vector<vector<bool>> grid;
+        for (int i=0; i<board.size(); i++) grid.push_back(vector<bool>(board[0].size(), false)); 
+        
+        for (int r=0; r<board.size(); r++) {
+            for (int c=0; c<board[0].size(); c++) {
+                if (board[r][c] == word[0]) {
+                    grid[r][c] = true;
+                    if (traverse(board, grid, make_pair(r, c), word, 1)) return true;
+                    grid[r][c] = false;
+                }
+            }
+        }
+        return false;
+    }
+};
