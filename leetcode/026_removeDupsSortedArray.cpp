6328bedfdd5c79b72cb4c60d83eea28cd6538b34
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-05-15 22:09:52 -0700

    Sorted array problems.

---
+++ leetcode/026_removeDupsSortedArray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() <= 1) return nums.size(); 
+        int check=1, copy=0;
+        for (; check < nums.size(); check++) {
+            if (nums[check] == nums[copy]) continue;
+            nums[++copy] = nums[check];
+        }
+        return (copy+1);
+    }
+};
---
+++ leetcode/081_searchRotatedSortedArrayDups.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    int binarySearch(vector<int>& nums, int val, int lo, int hi) {
+        if (lo >= hi) return (val == nums[lo] ? lo : -1);
+        int mid = (lo + hi)/2;
+        int midplus = mid+1;
+        bool monoIncLhs = nums[lo] <= nums[mid];
+        bool monoIncRhs = nums[midplus] <= nums[hi];
+        if (nums[lo] == nums[mid] && nums[hi] == nums[midplus]) { //don't really know which part to search, so look in both
+            if (nums[lo] == val) return lo;
+            if (nums[hi] == val) return hi;
+            int searchlhs = binarySearch(nums, val, lo, mid-1);
+            int searchrhs = binarySearch(nums, val, mid+1, hi);
+            if (searchlhs != -1) return searchlhs;
+            if (searchrhs != -1) return searchrhs;
+            return -1;
+        }
+        
+        if ((monoIncLhs && val >= nums[lo] && val <= nums[mid]) ||
+            (!monoIncLhs && (val >= nums[lo] || val <= nums[mid]))) { //recurse on LHS
+            return binarySearch(nums, val, lo, mid);
+        } else if ((monoIncRhs && val >= nums[midplus] && val <= nums[hi]) ||
+                   (!monoIncRhs && (val >= nums[midplus] || val <= nums[hi]))){ //recurse on RHS
+            return binarySearch(nums, val, midplus, hi);
+        }
+        return -1;//else number not present
+    }
+
+public:
+    bool search(vector<int>& nums, int target) {
+        return (binarySearch(nums, target, 0, nums.size()-1) == -1 ? false : true);
+    }
+};
