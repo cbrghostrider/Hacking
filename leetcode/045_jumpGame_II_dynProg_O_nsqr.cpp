41edca6e3266506c5771db1166d52d61dce490de
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-05-23 18:31:17 -0700

    Straightforward O(n^2) dynamic programming.

---
+++ leetcode/045_jumpGame_II_dynProg_O_nsqr.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+        //dynamic programming cache
+        vector<int> numJumps(nums.size(), 0);
+        
+        //dynamic programming loop
+        for (int i=nums.size()-2; i>=0; i--) {
+            if (nums[i] <= 0) {
+                numJumps[i] = -1; //not possible
+                continue;
+            }
+            int minJumps = std::numeric_limits<int>::max();
+            bool foundMin = false;
+            for (int ind=i+1; ind<=i+nums[i] && ind < nums.size(); ind++) {
+                if (numJumps[ind] == -1) continue;
+                int theseJumps = 1 + numJumps[ind];
+                if (theseJumps <= minJumps) {
+                    minJumps = theseJumps;
+                    foundMin = true;
+                }
+            }
+            if (foundMin) {
+                numJumps[i] = minJumps;
+            } else {
+                numJumps[i] = -1; //could not find any way to get to end from here
+            }
+        }
+        
+        return numJumps[0];
+    }
+};
