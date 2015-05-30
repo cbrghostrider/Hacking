19658114dc64b17cbb7f054bdd4813d76b929e83
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-05-29 20:07:11 -0700

    O(n^2) algorithm; need to optimize.

---
+++ leetcode/214_shortestPalindrome_O_nsqr.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    //will return whether string can be made palindrome at that pivot (from the end)
+    //if so, will make it a palindrome
+    bool isPalindrome (string& sp, int pivot, bool isEven) {
+        if (pivot == 0) {
+            string attach = sp.substr(1);
+            std::reverse(attach.begin(), attach.end());
+            sp = attach + sp;
+            return true;
+        }
+        int i=pivot, j = isEven ? pivot-1 : pivot;
+        for (; i<sp.length() && j >= 0; i++, j--) {
+            if (sp[i] != sp[j]) { return false;} 
+        }
+        string attach = sp.substr(i);
+        std::reverse(attach.begin(), attach.end());
+        sp = attach + sp;
+        return true;
+    }
+    
+public:
+    string shortestPalindrome(string s) {
+        if (s.length() <= 1) return s;
+        bool isEven = (s.length() % 2) == 0;
+
+        for (int pivot = s.length()/2; pivot>=0; ) {
+            string sp = s;
+            if (isPalindrome(sp, pivot, isEven)) return sp;
+            if (isEven) pivot--;
+            isEven = !isEven;
+        }
+        assert(0);     
+    }
+};
