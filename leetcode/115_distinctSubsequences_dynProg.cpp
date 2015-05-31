d11538b54838743991eff97f90f6670fcfc61f8e
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-05-30 22:43:18 -0700

    A couple of dynamic programming problems.

---
+++ leetcode/115_distinctSubsequences_dynProg.cpp
@@ -0,0 +1,22 @@
+// O(mn) space and O(mn) time
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        //dynamic programming cache
+        vector<vector<int>> cache(s.length()+1, vector<int>(t.length()+1, 0));
+        
+        //base cases
+        cache[0][0] = 1;                             //all s and t letters lined up perfectly w/ this recursion
+        for (int i=1; i<s.length(); i++) cache[i][0] = 1;  //some s letters were left, but all t letters consumed. Delete all s letters.
+        for (int i=1; i<t.length(); i++) cache[0][i] = 0;  //some t letters left over. This is invalid
+        
+        for (int si = 1; si <= s.length(); si++) {
+            for (int ti = 1; ti <= t.length(); ti++) {
+                int cand1 = (s[si-1] == t[ti-1]) ? cache[si-1][ti-1] : 0; //matched this letter in s and t, try prev ones!
+                int cand2 = cache[si-1][ti];                              //delete this letter in s, and keep trying to match
+                cache[si][ti] = cand1 + cand2;
+            }
+        }
+        return cache[s.length()][t.length()];
+    }
+};
---
+++ leetcode/140_wordBreak_II_dynProg.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private:
+    vector<vector<int> > phrases_; 
+    vector<string> constructRetval(const string& s, int ind) {
+        if (ind < 0 ) return vector<string>(1, ""); 
+        vector<string> retval;
+        for (const int& start : phrases_[ind]) {
+            vector<string> recurse = constructRetval(s, start-1);
+            for (string ph : recurse) {
+                string myword = s.substr(start, ind-start+1);
+                ph = (ph == "") ? myword : ph + " " + myword;
+                retval.push_back(ph);
+            }
+        }
+        return retval;
+    }
+public:
+    vector<string> wordBreak(string s, unordered_set<string>& wordDict) {
+        //allocate cache
+        for (unsigned int i=0; i<s.size(); i++) {
+            vector<int> cache; //start indices for word ending at my index
+            phrases_.push_back(cache);
+        }
+        
+        //main dynamic programming algorithm
+        for (unsigned int i=0; i<s.length(); i++) { 
+            // is [0,i] one huge word?
+            string testWholeWord = s.substr(0, i+1);
+            if (wordDict.find(testWholeWord) != wordDict.end()) {
+                phrases_[i].push_back(0);
+            }
+                
+            for (unsigned int j=0; j<i; j++) {
+                //if [0, j] is a bunch of words, and [j+1, i] is a word, we are good!
+                string testMe = s.substr(j+1, i-j);
+                if ((wordDict.find(testMe) != wordDict.end()) && !phrases_[j].empty()) {
+                    phrases_[i].push_back(j+1);
+                }
+            }
+        }
+        return constructRetval(s, s.length()-1);
+    }
+};
