5280e4660974b0ca08c6c3ebf21fc7d2b47e0ef3
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-06-03 21:34:35 -0700

    Convex hull solution in Haskell.

---
+++ HackerRank/FunctionalProgramming/Recursion/convexHull.hs
@@ -0,0 +1,68 @@
+import Text.Printf
+import Data.List
+import Data.Ord
+
+type Coord = (Int, Int)
+
+findBottomRCornerPt :: [Coord] -> Coord
+findBottomRCornerPt = foldl1' (\accpt pt -> if snd pt < snd accpt then pt else if snd pt > snd accpt then accpt else if fst pt > fst accpt then pt else accpt)
+
+-- takes two vectors and finds the angle between them
+angleBetween :: Coord -> Coord -> Double
+angleBetween v1@(x1, y1) v2@(x2, y2) = 
+  let v1mag = sqrt . fromIntegral $ (x1*x1 + y1*y1) 
+      v2mag = sqrt . fromIntegral $ (x2*x2 + y2*y2)
+      uv1   = ((fromIntegral x1) / v1mag, (fromIntegral y1) / v1mag)
+      uv2   = ((fromIntegral x2) / v2mag, (fromIntegral y2) / v2mag)
+      uvdotprod = (fst uv1 * fst uv2) + (snd uv1 * snd uv2)
+      angle = acos uvdotprod
+  in  if (x1 == 0 && y1 == 0) || (x2 == 0 && y2 == 0)then 0.0 else if uv1 == uv2 then 0.0 else angle
+
+computeAngleWith :: Coord -> Coord -> Double
+computeAngleWith lowPt myPt = angleBetween (fst myPt - fst lowPt, snd myPt - snd lowPt) unitxv
+  where unitxv = (1, 0)
+
+counterClockWise :: Coord -> Coord -> Coord -> Bool
+counterClockWise p1@(p1x, p1y) p2@(p2x, p2y) p3@(p3x, p3y) = ((p2x - p1x) * (p3y - p1y) - (p2y - p1y) * (p3x - p1x)) > 0
+
+-- takes a list of Coords sorted by their polar angle with bottom corner pt (which is head)
+-- returns a list of Coords on the convex hull perimeter
+grahamScan :: [Coord] -> [Coord] -> [Coord]
+grahamScan accpts []  = accpts
+grahamScan accpts pts = 
+  let initpt  = head . tail $ accpts
+      nextpt  = head accpts
+      thispt  = head pts
+      retval  =   if counterClockWise initpt nextpt thispt
+                     then grahamScan (thispt : accpts) (tail pts)
+                     else if length accpts == 2 
+                             then grahamScan (accpts) (tail pts)
+                             else grahamScan (tail accpts) pts 
+  in  retval
+
+perimeter :: [Coord] -> Double
+perimeter prms = distFinal + (fst $ foldl' (\acc@(rv, lpt) npt -> (rv + distance lpt npt, npt)) (0.0, head prms) (tail prms))
+  where
+    distFinal = distance (head prms) (last prms)
+    distance p1@(p1x, p1y) p2@(p2x, p2y) = let xdelta = p1x - p2x 
+                                               ydelta = p1y - p2y 
+                                           in  sqrt . fromIntegral $ (xdelta * xdelta + ydelta * ydelta)
+
+solve :: [Coord] -> Double
+solve points = 
+  let pts    = nub points
+      ptBR   = findBottomRCornerPt pts
+      ptRest = sortBy (comparing (computeAngleWith ptBR)) $ pts \\ [ptBR]
+      prm    = grahamScan ((head ptRest) : [ptBR]) ((tail ptRest) )
+  in  perimeter prm
+
+main :: IO ()
+main = do
+  n <- readLn :: IO Int
+  content <- getContents
+  let  
+    points = map (\[x, y] -> (x, y)). map (map (read::String->Int)). map words. lines $ content
+    ans = solve points
+  printf "%.1f\n" ans
+
+
