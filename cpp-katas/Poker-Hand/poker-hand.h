c7c23bbc57e1b9fc8cd5f75cf4cdadb828c88006
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-06-06 21:39:12 -0700

    Copyright notice on cpp kata solutions.

--- cpp-katas/Poker-Hand/poker-hand.h
+++ cpp-katas/Poker-Hand/poker-hand.h
@@ -1,3 +1,8 @@
+// -------------------------------------------------------------------------------------
+//         Author: Sourabh S Joshi (cbrghostrider); Copyright - All rights reserved.
+//                       For email, run on linux (perl v5.8.5):
+//   perl -e 'print pack "H*","736f75726162682e732e6a6f73686940676d61696c2e636f6d0a"'
+// -------------------------------------------------------------------------------------
 // Author - Sourabh S Joshi
 #ifndef POKER_HAND_H_
 #define POKER_HAND_H_
