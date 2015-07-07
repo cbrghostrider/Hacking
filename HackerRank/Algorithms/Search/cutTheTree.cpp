8eb7af9634bff8d5fccf4e077db14c5d805a68bf
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-07-06 21:36:28 -0700

    Rewrote using adjacency lists instead of pointers.

--- HackerRank/Algorithms/Search/cutTheTree.cpp
+++ HackerRank/Algorithms/Search/cutTheTree.cpp
@@ -4,11 +4,6 @@
 //   perl -e 'print pack "H*","736f75726162682e732e6a6f73686940676d61696c2e636f6d0a"'
 // -------------------------------------------------------------------------------------
 // 
-// Input format for this problem is messed up on hacker rank
-// maybe a graph, and not a tree as stated in problem statement
-// currently passing the first 6 tests, but timing out on rest
-// need to debug... including code here only for completeness
-//
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -18,14 +13,6 @@
 #include <unordered_set>
 using namespace std;
 
-struct TreeNode {
-    int val;
-    int sum;
-    bool visited;
-    vector<TreeNode*> children;
-    TreeNode(int value) : val(value), sum(0), visited(false), children() {} 
-};
-
 class Tree {
 public:
     Tree();
@@ -33,106 +20,79 @@ public:
     int getMinTreeDiff();
 private:
     void readInput();
-    int computeSum(TreeNode* node);
-    void minTreeDiff(TreeNode* root, const int total);
-    unsigned long long toKey(pair<int, int> nodes); 
-    pair<int, int> fromKey(unsigned long long key);
-    vector<TreeNode*> nodes;
+    int computeSum(int node);
+    void minTreeDiff(int root, const int total);
+    void clearFlags();
+
+    vector<int> vals;
+    vector<int> sums;
+    vector<bool> visited;
+    vector<unordered_set<int>> adjList;
     int minsofar;
 };
 
 Tree::Tree() 
-: nodes(), minsofar() {
+: vals(), sums(), visited(), adjList(), minsofar() {
     readInput();
 }
 
 Tree::~Tree() {
-    for (TreeNode*& node : nodes) {
-        delete node;
-    }
-    nodes.clear();
 }
 
-unsigned long long Tree::toKey(pair<int, int> nodes) {
-    return ((((unsigned long long)(nodes.first)) << 32) | ((unsigned long long)(nodes.second)));
-}
-pair<int, int> Tree::fromKey(unsigned long long key) {
-    return make_pair((key >> 32) & 0xFFFFFFFF, (key) & 0xFFFFFFFF);
+void Tree::clearFlags() {
+    for (int i=0; i<visited.size(); i++) visited[i] = false; 
 }
 
-int Tree::computeSum(TreeNode* root) {
-    if (root == nullptr) return 0;
+int Tree::computeSum(int node) {
+    visited[node] = true;
     int childrensum = 0;
-    for (TreeNode*& child : root->children) {
+    for (const int& child : adjList[node]) {
+        if (visited[child]) continue;
         childrensum += computeSum(child);
     }
-    root->sum = childrensum + root->val;
-    return root->sum;
+    sums[node] = childrensum + vals[node];
+    return sums[node];
 }
 
 int Tree::getMinTreeDiff() {
     minsofar = std::numeric_limits<int>::max();
-    computeSum(nodes[0]);
-    minTreeDiff(nodes[0], nodes[0]->sum); //assuming node 0 is root
+    clearFlags();
+    computeSum(0);
+    clearFlags();
+    minTreeDiff(0, sums[0]); //assuming node 0 is root
     return minsofar;
 }
 
-void Tree::minTreeDiff(TreeNode *root, const int total) {
-    if (root == nullptr) return;
-    for (TreeNode*& child : root->children) {
-        int childsum = child->sum;
-        int withoutchildsum = total - childsum;
-        int cand = abs(withoutchildsum - childsum);
+void Tree::minTreeDiff(int node, const int total) {
+    visited[node] = true;
+    for (const int& child : adjList[node]) {
+        if (visited[child]) continue;
+        int withoutchildsum = total - sums[child];
+        int cand = abs(withoutchildsum - sums[child]);
         if (cand < minsofar) minsofar = cand;
-        if ((withoutchildsum - childsum ) > minsofar) continue; //don't descend further
+        if ((withoutchildsum - sums[child] ) > minsofar) continue; //don't descend further
         minTreeDiff(child, total);
     }
 }
 
 void Tree::readInput() {
     int n; std::cin >> n;
-    unordered_set<unsigned long long> edges;
     
     for (int i=0; i<n; i++) {
         int value; std::cin >> value;
-        TreeNode* node = new TreeNode(value);
-        nodes.push_back(node);
+        vals.push_back(value);
     }
+
+    adjList = vector<unordered_set<int>>(n, unordered_set<int>());
+    visited = vector<bool>(n, false);
+    sums    = vector<int>(n, 0);
+
     for (int i=0; i<n-1; i++) {
         int n1, n2; std::cin >> n1 >> n2;
         n1 -= 1; n2 -= 1; //make 0 indexed
-        edges.insert(toKey(make_pair(n1, n2)));
+        adjList[n1].insert(n2);
+        adjList[n2].insert(n1);
     }
-    
-    int ctr = 0; //links made
-    unordered_set<int> parents, children; 
-    vector<bool> processed(n, false); //these guys have been processed
-    children.insert(0);
-    
-    while (ctr < n-1) {
-        parents = children; children.clear();
-        for (const int& parent : parents) {
-            unordered_set<unsigned long long> removeedges; //these ones are fully done
-            
-            for (auto edgekey : edges) {
-                auto edge = fromKey(edgekey);
-                if (edge.first != parent && edge.second != parent) continue;
-                int child = (edge.first == parent) ? edge.second : edge.first;
-                if (processed[child]) { //this was in fact, my parent's parent, not child
-                    removeedges.insert(toKey(edge));
-                    continue;
-                }
-                nodes[parent]->children.push_back(nodes[child]);
-                children.insert(child);    
-            }
-            processed[parent] = true;
-            for (auto redge : removeedges) {
-                edges.erase(redge);
-            }
-        }
-        ctr += children.size();
-    }
- 
 }
 
 int main() {
