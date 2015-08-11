12b3d24a99e148e30de2a948a8453cf9dfa3be84
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-08-10 20:22:22 -0700

    LOL.

---
+++ leetcode/242_validAnagram.cpp
@@ -0,0 +1,13 @@
+// -------------------------------------------------------------------------------------
+//         Author: Sourabh S Joshi (cbrghostrider); Copyright - All rights reserved.
+//                       For email, run on linux (perl v5.8.5):
+//   perl -e 'print pack "H*","736f75726162682e732e6a6f73686940676d61696c2e636f6d0a"'
+// -------------------------------------------------------------------------------------
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        return (s == t);
+    }
+};
