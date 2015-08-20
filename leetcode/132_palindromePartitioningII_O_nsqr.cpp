b5dd80514d600f5ac8b4fead35bfdba8f3ef90a0
Sourabh S Joshi <sourabh.s.joshi@gmail.com>
2015-08-19 21:01:25 -0700

    Quadratic algorithm for palindrome partitioning.

---
+++ leetcode/132_palindromePartitioningII_O_nsqr.cpp
@@ -0,0 +1,36 @@
+// -------------------------------------------------------------------------------------
+//         Author: Sourabh S Joshi (cbrghostrider); Copyright - All rights reserved.
+//                       For email, run on linux (perl v5.8.5):
+//   perl -e 'print pack "H*","736f75726162682e732e6a6f73686940676d61696c2e636f6d0a"'
+// -------------------------------------------------------------------------------------
+class Solution {
+    bool isPalindrome_(const string& s) {
+        for (int i=0; i<s.length()/2; i++) {
+            if (s[i] != s[s.length()-1-i]) return false;
+        }
+        return true;
+    }
+public:
+    int minCut(string s) {
+        if (s.length() == 0) return 0;
+
+        int cache[s.length()];
+        cache[0] = 0; //this letter by itself is already a palindrome, requires no mincuts
+
+        for (int ind=1; ind<s.length(); ind++) {
+            if (isPalindrome_(s.substr(0, ind+1))) {
+                cache[ind] = 0;
+                continue;
+            }
+            int mincut = std::numeric_limits<int>::max();
+            for (int brk=ind-1; brk>=0; brk--) {
+                string ss = s.substr(brk+1, ind+1);
+                if (!isPalindrome_(ss)) continue;
+                int cand = cache[brk] + 1;
+                if (cand < mincut) mincut = cand;
+            }
+            cache[ind] = mincut;
+        }
+        return cache[s.length() - 1];
+    }
+};
